f4d7f02106f2ba83ca6a3ff8bad38fe73d465002
Artem <artem.lyoshin@gmail.com>
2024-10-06 12:55:40 +0200

    add frtToCls

--- Converter/src/main/java/org/example/Main.java
+++ Converter/src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter");
+        System.out.println(frtToCls(212));
+    }
+    public static double frtToCls(int frt){
+        return (frt-32)/1.8;
     }
 }
