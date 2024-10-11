c00b09075f50ea9a197e1e712ab0fb81b013ccab
Artem <artem.lyoshin@gmail.com>
2024-10-11 11:32:32 +0200

    Completed!

---
+++ hw5/src/main/java/org/app/CalcCostBase.java
@@ -0,0 +1,10 @@
+package org.app;
+
+// Клас розрахунку базової вартості товару
+public class CalcCostBase {
+
+    // Базовий розрахунок вартості товару
+    public double calcCost(Product product) {
+        return product.getQuota() * product.getPrice();
+    }
+}
